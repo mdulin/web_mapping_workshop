9f57ae8b7d480b1a62fefdf9bcc5bea9b9f706b8
mdulin <michael.w.dulin@usace.army.mil>
2014-11-13 14:33:38 -0600

    Update app_2.js

--- js/app_2.js
+++ js/app_2.js
@@ -14,7 +14,7 @@ map.setView([39, -96], 4);
 ///////////////////////////////////////////////////////////////////////////
 // This is the area we're going to use to add data to our map
 
-var dataFileToAdd = 'data/bars.geojson'; //<- Point this to the file that you want to include on the map
+var dataFileToAdd = 'data/dots.geojson'; //<- Point this to the file that you want to include on the map
 var dataToAdd;
 
 var featureLayer = L.mapbox.featureLayer()
