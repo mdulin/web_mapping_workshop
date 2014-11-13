405e04d482c8922c0d91a43e1597f46364f780d6
mdulin <michael.w.dulin@usace.army.mil>
2014-11-13 15:23:08 -0600

    Update app_4.js

--- js/app_4.js
+++ js/app_4.js
@@ -59,7 +59,7 @@ var clickHandler = function(e){
   $('#info').fadeIn(400,function(){
     var info = '';
 
-    info = '<div>You clicked on the layer with the id of ' + feature.properties.PROJ_NAME + '</div>'
+    info = '<div>You clicked on the layer with the id of ' + feature.properties.PROJ_NAME + '</div>'+
     			'<p> Post to public site' + feature.properties.PUB_MEDIA +'</p>';
 
     $('#info').append(info);
